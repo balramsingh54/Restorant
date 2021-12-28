3e3ea07ba50250560bda589a3be9f05ce65f85b4
“Balram <balram.singh@startelelogic.com>
2021-12-28 13:50:10 +0530

    restorant

---
+++ resto/src/app/add-resto/add-resto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-resto',
+  templateUrl: './add-resto.component.html',
+  styleUrls: ['./add-resto.component.scss']
+})
+export class AddRestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
